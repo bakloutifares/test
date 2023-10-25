f5de7f2ae339cbe948b1e911d0a7b6e6b6f9948b
Fares BAKLOUTI <fares.baklouti@esprit.tn>
2023-10-25 17:50:19 +0100

    modification 2

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -23,7 +23,7 @@ public class User implements Serializable {
 
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
-	private Long id;   
+	private Long idsaasas;
 	
 	private String firstName; 
 	
